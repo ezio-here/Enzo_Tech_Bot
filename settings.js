dbec39a2acdd99aae6bfccd798d2ca2f55c3455b
Ezio-Here <5starbeatz3@gmail.com>
2025-12-16 23:31:48 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Enzo Tech bot',
+  author: '‎',
+  botName: "Enzo Tech Bot",
+  botOwner: 'Enzo Here', // Your name
+  ownerNumber: '923367542691', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.5",
+  updateZipUrl: "",
+};
+
+module.exports = settings;
